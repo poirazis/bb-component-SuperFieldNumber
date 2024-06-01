118b7707b8a66ce4d24f61f8733c0b9cade31f6a
poirazis <poirazis@gmail.com>
2024-06-01 21:41:26 +0300

    Moved to common css for all field components

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,7 +1,8 @@
 <script>
-  import { getContext , onDestroy} from "svelte";
+  import { getContext, onDestroy } from "svelte";
   import CellNumber from "../../bb_super_components_shared/src/lib/SuperTableCells/CellNumber.svelte";
-  
+  import "../../bb_super_components_shared/src/lib/SuperFieldsCommon.css";
+
   const { styleable, Provider, BlockComponent, Block } = getContext("sdk");
   const component = getContext("component");
 
@@ -9,40 +10,41 @@
   const formStepContext = getContext("form-step");
   const labelPos = getContext("field-group");
   const labelWidth = getContext("field-group-label-width");
+  const groupDisabled = getContext("field-group-disabled");
   const formApi = formContext?.formApi;
 
   export let field;
-  export let controlType
-  
-  export let customButtons
+  export let controlType;
+
+  export let customButtons;
   export let buttons = [];
   export let buttonsQuiet;
 
   export let label;
   export let span = 6;
-  export let placeholder
-  export let defaultValue
-  export let disabled
-  export let readonly
-  export let validation
+  export let placeholder;
+  export let defaultValue;
+  export let disabled;
+  export let readonly;
+  export let validation;
 
-  export let template
+  export let template;
 
-  export let onChange
-  export let debounced
-  export let debounceDelay
+  export let onChange;
+  export let debounced;
+  export let debounceDelay;
 
-  export let icon
-  export let clearValueIcon
+  export let icon;
+  export let clearValueIcon;
 
   let formField;
   let formStep;
   let fieldState;
   let fieldApi;
-  let fieldSchema
+  let fieldSchema;
   let value;
-  let cellState
-  
+  let cellState;
+
   $: formStep = formStepContext ? $formStepContext || 1 : 1;
 
   $: formField = formApi?.registerField(
@@ -53,7 +55,7 @@
     readonly,
     validation,
     formStep
-  )
+  );
 
   $: unsubscribe = formField?.subscribe((value) => {
     fieldState = value?.fieldState;
@@ -61,64 +63,59 @@
     fieldSchema = value?.fieldSchema;
   });
 
-  $: value = fieldState?.value ? fieldState.value : defaultValue
+  $: value = fieldState?.value ? fieldState.value : defaultValue;
 
-  $: cellOptions = { 
-      placeholder, 
-      defaultValue,
-      template,
-      disabled,
-      readonly: readonly || disabled,
-      icon,
-      error : fieldState?.error,
-      debounce: debounced ? debounceDelay : false,
-      clearValueIcon,
-      role: "formInput", 
-    }
+  $: cellOptions = {
+    placeholder,
+    defaultValue,
+    template,
+    disabled: disabled || groupDisabled || fieldState?.disabled,
+    readonly: readonly || fieldState?.readonly,
+    icon,
+    error: fieldState?.error,
+    debounce: debounced ? debounceDelay : false,
+    clearValueIcon,
+    role: "formInput",
+  };
 
   $: $component.styles = {
     ...$component.styles,
     normal: {
       ...$component.styles.normal,
       "flex-direction": labelPos == "left" ? "row" : "column",
-      gap: labelPos == "left" ? "0.85rem" : "0rem",
+      gap: labelPos == "left" ? "0.5rem" : "0rem",
       "grid-column": labelPos ? "span " + span : null,
       "--label-width":
         labelPos == "left" ? (labelWidth ? labelWidth : "6rem") : "auto",
     },
   };
-  
-  const handleChange = ( newValue ) => {
-    onChange?.({value: newValue});
+
+  const handleChange = (newValue) => {
+    onChange?.({ value: newValue });
     fieldApi?.setValue(newValue);
-  }
+  };
 
   onDestroy(() => {
-    fieldApi?.deregister()
-    unsubscribe?.()
-  })
+    fieldApi?.deregister();
+    unsubscribe?.();
+  });
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 <Block>
-  <div
-    class="superField"
-    on:focus={cellState.focus} 
-    tabindex="0"
-    use:styleable={$component.styles}  
-  >
+  <div class="superField" use:styleable={$component.styles}>
     {#if label}
-      <label for="superCell" class="superlabel" style:flex-direction={labelPos == "left" ? "column" : "row"}>
+      <label for="superCell" class="superlabel" class:left={labelPos == "left"}>
         {label}
         {#if fieldState.error}
-          <div class="error">
+          <div class="error" class:left={labelPos == "left"}>
             <span>{fieldState.error}</span>
           </div>
         {/if}
       </label>
     {/if}
-    
+
     <div class="inline-cells">
       <CellNumber
         bind:cellState
@@ -134,62 +131,24 @@
           class="spectrum-ActionGroup spectrum-ActionGroup--compact spectrum-ActionGroup--sizeM"
           class:spectrum-ActionGroup--quiet={buttonsQuiet}
         >
-          <Provider data={ {value} } >
-            {#each buttons as { text, onClick }}
+          <Provider data={{ value }}>
+            {#each buttons as { text, onClick, icon, disabled, quiet, type }}
               <BlockComponent
-                type = "plugin/bb-component-SuperButton"
-                props = {{
+                type="plugin/bb-component-SuperButton"
+                props={{
                   size: "M",
                   text,
-                  onClick
-                }}>
-                </BlockComponent>
-              {/each}
+                  icon,
+                  disabled,
+                  quiet,
+                  type,
+                  onClick,
+                }}
+              ></BlockComponent>
+            {/each}
           </Provider>
         </div>
       {/if}
     </div>
   </div>
 </Block>
-
-<style>
-  .superField {
-    display: flex;
-    align-items: stretch;
-    justify-content: stretch;
-    min-width: 0;
-  }
-
-  .superField:focus {
-    outline: none;
-  }
-  .superlabel {
-    display: flex;
-    justify-content: space-between;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    min-width: var(--label-width);
-    max-width: var(--label-width);
-    font-size: 13px;
-    line-height: 1.75rem;
-    font-weight: 400;
-    color: var(--spectrum-global-color-gray-700);
-  }
-
-  .inline-cells {
-    flex: 1;
-    display: flex;
-    justify-items: stretch;
-    height: 2rem;
-  }
-
-  .error {
-    font-size: 12px;
-    line-height: 1.75rem;
-    color: var(--spectrum-global-color-red-700);
-  }
-</style>
-
-
-
